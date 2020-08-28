4b18b0c6eb9044850a1b587b1ae93733cadeeae6
askycat <875387716@qq.com>
2020-08-28 13:47:11 +0800

    new

---
+++ token.sol
@@ -0,0 +1,126 @@
+pragma solidity ^0.4.22;
+
+contract Owner {
+
+    address private owner;
+    
+    // event for EVM logging
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+    
+    // modifier to check if caller is owner
+    modifier isOwner() {
+        // If the first argument of 'require' evaluates to 'false', execution terminates and all
+        // changes to the state and to Ether balances are reverted.
+        // This used to consume all gas in old EVM versions, but not anymore.
+        // It is often a good idea to use 'require' to check if functions are called correctly.
+        // As a second argument, you can also provide an explanation about what went wrong.
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+        emit OwnerSet(address(0), owner);
+    }
+
+
+    function changeOwner(address newOwner) public isOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+}
+
+library SafeMath {
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b);
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
+        
+	return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a);
+
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0);
+        return a % b;
+    }
+}
+
+contract ERC20Token is Owner {
+    
+    using SafeMath for uint256;
+
+	uint   constant public totalSupply=100000000e18;
+	string constant public name="token name";
+    string constant public symbol="token";
+	uint8  constant public decimals=18;
+	
+	mapping (address => uint256) balances;
+	mapping (address => mapping (address => uint)) allowed;
+
+	
+    constructor () public {
+		balances[msg.sender] = totalSupply;
+    }
+    
+	//Fix for short address attack against ERC20
+	modifier onlyPayloadSize(uint size) {
+		assert(msg.data.length == size + 4);
+		_;
+	} 
+
+	function balanceOf(address _owner) public view returns (uint balance) {
+		return balances[_owner];
+	}
+	function transfer(address _recipient, uint _value) public onlyPayloadSize(2*32) {
+	    require(balances[msg.sender] >= _value && _value > 0);
+	    balances[msg.sender] = balances[msg.sender].sub(_value);
+	    balances[_recipient] = balances[_recipient].add(_value);
+	    emit Transfer(msg.sender, _recipient, _value);        
+        }
+	function transferFrom(address _from, address _to, uint _value) public {
+	    require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
+            balances[_to] = balances[_to].add(_value);
+            balances[_from] = balances[_from].sub(_value);
+            allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+            emit Transfer(_from, _to, _value);
+        }
+	function approve(address _spender, uint _value) public {
+		allowed[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
+	}
+	function allowance(address _spender, address _owner) public view returns (uint balance) {
+		return allowed[_owner][_spender];
+	}
+    
+	event Transfer(address indexed _from,address indexed _to,uint _value);
+	event Approval(	address indexed _owner,address indexed _spender,uint _value	);
+}
